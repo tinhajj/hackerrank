e5eecc2cd4291ab952d8a33ac6c6de47cafdfbe9
Thanoj Singh <thanoj.singh@gmail.com>
2016-12-29 13:11:31 -0500

    Finished coin change problem

--- coin_problem/coin_problem.rb
+++ coin_problem/coin_problem.rb
@@ -3,7 +3,9 @@ def coin_change(coins, goal)
 end
 
 def coin_change_helper(coins, goal, index, map)
-  if goal == 0
+  if map["#{goal} - #{index}"]
+    return map["#{goal} - #{index}"]
+  elsif goal == 0
     return 1
   elsif index > coins.length - 1
     return 0
@@ -11,18 +13,21 @@ def coin_change_helper(coins, goal, index, map)
 
   remaining = goal
   current_coin = coins[index]
-  ways = []
 
-  while remaining >= current_coin or remaining == 0
-    ways << coin_change_helper(coins, remaining, index + 1, map)
+  ways_total = []
+
+  while remaining >= current_coin or remaining >= 0
+    ways_subproblem = coin_change_helper(coins, remaining, index + 1, map)
+    map["#{remaining} - #{index + 1}"] = ways_subproblem
+    ways_total << ways_subproblem
+
     remaining = remaining - current_coin
   end
 
-  ways.inject(&:+)
+  ways_total.inject(&:+)
 end
 
-coins = [3, 2, 1]
-target_goal = 4
+#target = gets.split(' ')[0].to_i
+#coins = gets.split(' ').map(&:to_i)
 
-p coin_change(coins, target_goal)
-
+p coin_change(coins, target)
